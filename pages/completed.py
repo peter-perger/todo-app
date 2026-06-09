5937a610e99f9238fcfbb8d55015e394413130b6
Peter Perger <pergerpeti2@gmail.com>
2026-06-09 11:56:25 +0200

    Changed teh display of completed todos number

--- pages/completed.py
+++ pages/completed.py
@@ -2,8 +2,7 @@ from functions import get_todos
 import streamlit as st
 
 completed_todos = get_todos("files/completed.txt")
-st.title("Completed todos✅")
-st.subheader(f"({len(completed_todos)})")
+st.title(f"Completed todos✅ ({len(completed_todos)})")
 
 for index, todo in enumerate(completed_todos):
     st.text(todo)
